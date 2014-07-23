03e19a430c68511dbf5939c43d9ac588f63a4ecc
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2014-07-24 01:31:20 +0530

    Intial commit for sample application.

---
+++ CSharpHashSet/CSharpHashSet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            HashSet<string> nameHashSet=
+                new HashSet<string>
+                {
+                    "Jalpesh", "Vishal", "Tushar", "Jalpesh"
+                };
+
+
+            nameHashSet.Add("Chirag");
+            nameHashSet.Remove("Tushar");
+
+            foreach (var item in nameHashSet)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
